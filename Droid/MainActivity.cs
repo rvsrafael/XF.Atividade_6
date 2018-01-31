f8e8303030ea2f12e3b18d3aba131ccbb9d33dbd
rvsrafael <rvs.contato@gmail.com>
2018-01-31 17:09:50 -0200

    estrutura

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Microsoft.WindowsAzure.MobileServices;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using static FiapAtividade6.App;
+using System.Threading.Tasks;
+
+namespace FiapAtividade6.Droid
+{
+	[Activity (Label = "FiapAtividade6.Droid",
+		Icon = "@drawable/icon",
+		MainLauncher = true,
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+		Theme = "@android:style/Theme.Holo.Light")]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity, IAuthenticate	
+    {
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Initialize Azure Mobile Apps
+			CurrentPlatform.Init();
+
+			// Initialize Xamarin Forms
+			Forms.Init (this, bundle);
+
+            // Initialize the authenticator before loading the app.
+            App.Init((IAuthenticate)this);
+
+			// Load the main application
+			LoadApplication (new App ());
+		}
+
+
+        // Define a authenticated user.
+        private MobileServiceUser user;
+
+        public async Task<bool> Authenticate()
+        {
+            var success = false;
+            var message = string.Empty;
+            try
+            {
+                // Sign in with Facebook login using a server-managed flow.
+                user = await TodoItemManager.DefaultManager.CurrentClient.LoginAsync(this,
+                MobileServiceAuthenticationProvider.Facebook, "https://fiapatividade6.azurewebsites.net");
+                if (user != null)
+                {
+                    message = string.Format("you are now signed-in as {0}.",
+                        user.UserId);
+                    success = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+
+            // Display the success or failure message.
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.SetMessage(message);
+            builder.SetTitle("Sign-in result");
+            builder.Create().Show();
+
+            return success;
+        }
+	}
+}
+
---
+++ FiapAtividade6/App.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FiapAtividade6
+{
+	public class App : Application
+	{
+        public interface IAuthenticate
+        {
+            Task<bool> Authenticate();
+        }
+
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new TodoList();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+
+        public static IAuthenticate Authenticator { get; private set; }
+
+        public static void Init(IAuthenticate authenticator)
+        {
+            Authenticator = authenticator;
+        }
+	}
+}
+
